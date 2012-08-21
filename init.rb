e49c5b8b4723568076468279ca8bb8969c5a00e5
John Wyatt <jbwyatt4@gmail.com>
2012-08-21 00:27:34 -0700

    added to changelog

--- init.rb
+++
@@ -1,8 +0,0 @@
-require 'metrozd'
-
-#http://stackoverflow.com/questions/1878640/including-rake-tasks-in-gems
-#Dir["#{RAILS_ROOT}/vendor/plugins/*/tasks/**/*.rake"].sort.each { |ext| load ext }
-#Dir["#{RAILS_ROOT}/vendor/plugins/*/lib/tasks/**/*.rake"].sort.each { |ext| load ext }
-
-Dir["#{File.dirname(__FILE__)}/tasks/*.rake"].sort.each { |ext| load ext }
-
